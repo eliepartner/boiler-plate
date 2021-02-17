bcb6a05c55eecd157ebe89fb149fc5264f6c1b93
eliepartner <dev@elieorgel.com>
2021-02-17 05:58:43 -0500

    mongodb setup

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI: 'mongodb+srv://eliepartner:IMissingJasmine830~@boilderplate.w27gx.mongodb.net/boilerplate?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 const express = require('express');
 const app = express();
+
+const config = require('./config/key');
+
 const port = 5000;
+
+const mongoose_con = require('mongoose');
+mongoose_con.connect(config.mongoURI, {
+    useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
+}).then(() => console.log('MongoDB connected...'))
+    .catch(err => console.log(err))
 
 app.get('/', (req, res) => {
     res.send('Hello Coy Jennings!');
