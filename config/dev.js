d3910949b9193aaa971173e4a38ae57b465fd606
eliepartner <dev@elieorgel.com>
2021-02-17 11:20:23 -0500

    Body Parser & register

--- config/dev.js
+++
@@ -1,3 +0,0 @@
-module.exports = {
-    mongoURI: 'mongodb+srv://eliepartner:IMissingJasmine830~@boilderplate.w27gx.mongodb.net/boilerplate?retryWrites=true&w=majority'
-}
--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
 const config = require('./config/key');
 
 const port = 5000;
+
+const { User } = require('./models/User');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.json());
 
 const mongoose_con = require('mongoose');
 mongoose_con.connect(config.mongoURI, {
@@ -13,6 +19,18 @@ mongoose_con.connect(config.mongoURI, {
 
 app.get('/', (req, res) => {
     res.send('Hello Coy Jennings!');
+})
+
+app.post('/register', (req, res) => {
+    const user = new User(req.body);
+
+    user.save((err, user) => {
+        if (err) {
+            return res.json({ success:false, err:err })
+        } else {
+            return res.status(200).json({ success: true })
+        }
+    });
 })
 
 app.listen(port, () => {
