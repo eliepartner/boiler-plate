4b03dee133fd263d8ade2f177c37446bd6a26d49
eliepartner <dev@elieorgel.com>
2021-02-18 04:11:09 -0500

    login page

--- client/src/components/views/LandingPage/LandingPage.js
+++ client/src/components/views/LandingPage/LandingPage.js
@@ -8,8 +8,10 @@ function LandingPage() {
     }, []);
 
     return (
-        <div>
-            LandingPage
+        <div style={{
+            display: 'flex', justifyContent: 'center', alignItems: 'center', width: '100%', height: '100vh'
+        }}>
+            <h2>LandingPage</h2>
         </div>
     )
 }
